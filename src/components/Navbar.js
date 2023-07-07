eb09ac9191384570b6ef0abb4bfebb26050565e3
Francesca Bentin <francescabentin@gmail.com>
2023-07-07 13:45:57 +0200

    render products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import '../styles/layout/_navbar.scss'
 import logo from '../styles/assets/menu.png'
+import cart from '../styles/assets/cart.png'
 
 function Navbar() {
     return (
         <navbar className='navbar'>
             <div className='navbar__logo'>
-                <img src={logo} style={{ width: '60px', height: '50px' }}></img>
+                {/*<img src={logo} style={{ width: '50px', height: '40px' }}></img>*/}
+                <h1>SUMMERBALL</h1>
             </div>
             <ul className='navbar__list'>
                 <li><p>Hello Guest</p></li>
                 <li><button>Sign in</button></li>
-                <li><img src={logo} style={{ width: '60px', height: '50px' }}></img></li>
+                <li><img src={cart} style={{ width: '60px', height: '50px' }}></img></li>
             </ul>
         </navbar >
     );
