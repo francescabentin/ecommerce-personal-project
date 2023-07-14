9186b448c09a73c4f16d45624b58dfa18d6df294
Francesca Bentin <francescabentin@gmail.com>
2023-07-14 15:14:01 +0200

    add responsive hamb menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,33 @@ import '../styles/layout/_navbar.scss'
 //import logo from '../styles/assets/menu.png'
 import cart from '../assets/cart.png'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
+
+    const [isMenuVisible, setMenuVisible] = useState(false);
+
+    const handleClickEvent = () => {
+        setMenuVisible(!isMenuVisible);
+    };
+
+    const handleClickItem = () => {
+        setMenuVisible(false);
+    } 
+
     return (
         <section className='navbar'>
             <div className='navbar__logo' >
                 {/*<img src={logo} style={{ width: '50px', height: '40px' }}></img>*/}
                 <Link to="/"> <h1>SUMMERBALL</h1> </Link>
             </div>
-            <ul className='navbar__list hidden'>
+            <ul className={`navbar__list ${isMenuVisible ? 'show' : ''}`}>
                 <li><p>Hello Guest</p></li>
-                <li><Link to="login">login</Link></li>
-                <li><Link to="signup">signup</Link></li>
-                <li><Link to="my-cart"><img src={cart} style={{ width: '60px', height: '50px' }} alt='carrito de compras'></img></Link></li>
+                <li><Link onClick={handleClickItem} to="login">login</Link></li>
+                <li><Link onClick={handleClickItem} to="signup">signup</Link></li>
+                <li><Link onClick={handleClickItem} to="my-cart"><img src={cart} style={{ width: '60px', height: '50px' }} alt='carrito de compras'></img></Link></li>
             </ul>
-            <span className='show'>menu</span>
+            <span onClick={handleClickEvent} className='menuhidden'>menu</span>
         </section >
     );
 }
