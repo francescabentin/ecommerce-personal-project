330b0b20a6e78ec28f9694502cc2a3b97f56550f
Francesca Bentin <francescabentin@gmail.com>
2023-10-24 19:34:44 +0200

    fix basename browser-router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="ecommerce-personal-project/">
     <App />
   </BrowserRouter>
 );
