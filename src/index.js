0b4acd63ff3c2d23c7d91f81e29143773d30ec29
Francesca Bentin <francescabentin@gmail.com>
2023-11-25 13:27:32 +0100

    fix style and error message closing menus

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import store from "../src/store/store";
 import { Provider } from 'react-redux';
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
-    <BrowserRouter /*basename='/ecommerce-personal-project/'*/>
-    <App />
-  </BrowserRouter>
+    <HashRouter basename='/ecommerce-personal-project/'>
+      <App />
+    </HashRouter>
   </Provider >
 );
